c88d2d9072bb6dd13bacc98f8050840582c0129c
tomi <hering.tamas70@gmail.com>
2022-11-19 20:06:13 +0100

    Complete ImagesController to list metadatas of images and to get an image by id

--- src/main/java/hu/ponte/hr/controller/ImagesController.java
+++ src/main/java/hu/ponte/hr/controller/ImagesController.java
@@ -1,16 +1,22 @@
 package hu.ponte.hr.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Optional;
 
-import hu.ponte.hr.services.ImageStore;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-import java.util.Collections;
-import java.util.List;
+import hu.ponte.hr.model.Image;
+import hu.ponte.hr.services.ImageStore;
 
 @RestController()
 @RequestMapping("api/images")
@@ -21,12 +27,17 @@ public class ImagesController {
 
     @GetMapping("meta")
     public List<ImageMeta> listImages() {
-		return
-				List.of(new ImageMeta("1","first","pdf",1L,"sign"));
+		return imageStore.getMetaDatas();
     }
 
     @GetMapping("preview/{id}")
-    public void getImage(@PathVariable("id") String id, HttpServletResponse response) {
+    public void getImage(@PathVariable("id") String id, HttpServletResponse response) throws IOException {
+		Optional<Image> optionalImage = imageStore.findImageById(Long.valueOf(id));
+		if (optionalImage.isPresent()) {
+			Image image = optionalImage.get();
+			response.setContentType(image.getMimeType());
+			InputStream inputStream = new ByteArrayInputStream(image.getContent());
+			IOUtils.copy(inputStream, response.getOutputStream());
+		}
 	}
-
 }
